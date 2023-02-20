0f20a97fd0853707f7342e2696601683843e6f12
Lafaum <125826311+Lafaum1@users.noreply.github.com>
2023-02-20 13:34:55 -0300

    Create Tabuada.py

---
+++ Tabuada.py
@@ -0,0 +1,27 @@
+#tabuada
+
+result = int(input('Digite um número para descobrir sua tabuada: '))
+
+ta1 = result * 1
+ta2 = result * 2
+ta3 = result * 3
+ta4 = result * 4
+ta5 = result * 5
+ta6 = result * 6
+ta7 = result * 7
+ta8 = result * 8
+ta9 = result * 9
+ta10 = result * 10
+
+print('-' * 12)
+print(f'{result} x 01 = {ta1:2}')
+print(f'{result} x 02 = {ta2:2}')
+print(f'{result} x 03 = {ta3:2}')
+print(f'{result} x 04 = {ta4:2}')
+print(f'{result} x 05 = {ta5:2}')
+print(f'{result} x 06 = {ta6:2}')
+print(f'{result} x 07 = {ta7:2}')
+print(f'{result} x 08 = {ta8:2}')
+print(f'{result} x 09 = {ta9:2}')
+print(f'{result} x 10 = {ta10:2}')
+print('-' * 12)
