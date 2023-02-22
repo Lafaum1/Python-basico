f8f5c872562056dbc4c201075ce3942709d19edb
Lafaum <125826311+Lafaum1@users.noreply.github.com>
2023-02-22 14:20:38 -0300

    Update lerde0a9999.py

--- lerde0a9999.py
+++ lerde0a9999.py
@@ -1,29 +1,13 @@
-# Faça um programa que leia um número de 0 a 9999 e mostre na tela cada um dos dígitos separados.
-
+#Faça um programa que leia um número de 0 a 9999 e mostre na tela cada um dos dígitos separados.
 
 # Recebe o número digitado
-num_1 = input('Digite um número: ')
+num_1 = int(input('Digite um número: '))
 
-# Converte para inteiro e puxa o dígito da casa indicada.
-unid = int(num_1[0])
-
-# Verifica se a string tem pelo menos dois caracteres antes de acessar o segundo caractere. Caso contrario ele exibe 0
-if len(num_1) >= 2:
-    dez = int(num_1[1])
-else:
-    dez = 0
-
-# Verifica se a string tem pelo menos três caracteres antes de acessar o terceiro caractere. Caso contrario ele exibe 0
-if len(num_1) >= 3:
-    cen = int(num_1[2])
-else:
-    cen = 0
-
-# Verifica se a string tem pelo menos quatro caracteres antes de acessar o quarto caractere. Caso contrario ele exibe 0
-if len(num_1) >= 4:
-    mil = int(num_1[3])
-else:
-    mil = 0
+#Operação matématica
+unid = num_1 // 1 % 10
+dez = num_1 // 10 % 10
+cen = num_1 // 100 % 10
+mil = num_1 // 1000 % 10
 
 # Exibe as informações
 print(f'Analisando o número {num_1}. ')
