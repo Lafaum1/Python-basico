6bc70c11f6ddfc60a2b41b0844cc66d294d252e5
Lafaum <125826311+Lafaum1@users.noreply.github.com>
2023-02-24 12:08:57 -0300

    Create calculo-de-triangulo.py

---
+++ calculo-de-triangulo.py
@@ -0,0 +1,19 @@
+#Desenvolva um programa que leia o comprimento de três retas e diga ao usuário se elas podem ou não formar um triângulo.
+
+
+#Exibe layout
+print('-=-'*20)
+print('ANALISADOR DE TRIANGULOS')
+print('-=-'*20)
+
+#Puxa informações
+seg_1 = float(input('Qual o primeiro segmento? ').strip())
+seg_2 = float(input('Qual o segundo segmento? ').strip())
+seg_3 = float(input('Qual o terceiro segmento? ').strip())
+
+
+#Condição para calcular se é ou não um triangulo.
+if seg_1 < seg_2 + seg_3 and seg_2 < seg_1 + seg_3 and seg_3 < seg_1 + seg_2:
+    print(f' Os segmentos podem formar um triângulo. ')
+else:
+    print(f' Os segmentos NÃO podem formar um triângulo.')
